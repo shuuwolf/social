7e955ed3c5df9a0fb0d840b8b508f0e3279cef54
ShuuWolf <shuuwolf.home@gmail.com>
2025-03-05 21:16:05 -0300

    Add: env variables to .env

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -16,8 +16,10 @@ type application struct {
 }
 
 type config struct {
-	addr string
-	db   dbConfig
+	addr   string
+	db     dbConfig
+	env    string
+	apiURL string
 }
 
 type dbConfig struct {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,13 +16,15 @@ func main() {
 	}
 
 	cfg := config{
-		addr: env.GetString("ADDR", ":9999"),
+		addr:   env.GetString("ADDR", ":8080"),
+		apiURL: env.GetString("EXTERNAL_URL", "localhost:8080"),
 		db: dbConfig{
 			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
+		env: env.GetString("ENV", "development"),
 	}
 
 	db, err := db.New(
